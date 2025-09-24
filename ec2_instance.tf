0796507a5db88548ce1614a5feb59c909efa4ebe
kushal <kushall1845@gmai.com>
2025-09-24 18:51:13 +0530

    Added provisioner block to install ansible

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -10,6 +10,28 @@ resource "aws_instance" "server-01" {
 
     #user_data = file("userdata.sh")
 
+
+    provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y software-properties-common",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install -y ansible"
+    ]
+  }
+
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu" # Use "ec2-user" for Amazon Linux
+    private_key = local_file.private_key_pem.content
+    host        = self.public_ip
+  }
+
+
+
+    
+
     tags = {
 
         Name = "Server-01"
@@ -19,6 +41,22 @@ resource "aws_instance" "server-01" {
     depends_on = [ aws_internet_gateway.vpc-01-igw , local_file.private_key_pem ]
   
 }
+
+
+
+/* provisioner "remote-exec" {
+  inline = [
+    "sudo apt update -y",
+    "sudo apt install -y python3 python3-distutils curl",
+    "curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py",
+    "sudo python3 get-pip.py",
+    "sudo pip3 install --upgrade pip",
+    "sudo pip3 install ansible"
+  ]
+}
+ */
+
+
 
 
 
