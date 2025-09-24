6e3dc5c67e508bf4805ce33856e46e8104ed7fbe
kushal <kushall1845@gmai.com>
2025-09-24 21:40:22 +0530

    Modified provisioner block to install pip also

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,9 +1,9 @@
 
-resource "aws_instance" "server-01" {
+resource "aws_instance" "server_01" {
 
     ami           = "ami-0cfde0ea8edd312d4"  
     instance_type = "t3.micro"
-    subnet_id     = aws_subnet.vpc-01-subnet-01.id
+    subnet_id     = aws_subnet.vpc_01_subnet_01.id
     associate_public_ip_address = true
     
    key_name      = aws_key_pair.generated_key.key_name
@@ -12,14 +12,14 @@ resource "aws_instance" "server-01" {
 
 
     provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y software-properties-common",
-      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
-      "sudo apt install -y ansible"
-    ]
-  }
-
+  inline = [
+    "sudo apt update -y",
+    "sudo apt install -y python3 python3-pip python3-venv python3-full",
+    "sudo python3 -m pip install --upgrade pip",
+    "sudo python3 -m pip install --break-system-packages ansible",
+    "ansible --version"
+  ]
+}
 
   connection {
     type        = "ssh"
@@ -38,23 +38,14 @@ resource "aws_instance" "server-01" {
 
     }
 
-    depends_on = [ aws_internet_gateway.vpc-01-igw , local_file.private_key_pem ]
+    depends_on = [ aws_internet_gateway.vpc_01_igw , local_file.private_key_pem ]
   
 }
 
 
 
-/* provisioner "remote-exec" {
-  inline = [
-    "sudo apt update -y",
-    "sudo apt install -y python3 python3-distutils curl",
-    "curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py",
-    "sudo python3 get-pip.py",
-    "sudo pip3 install --upgrade pip",
-    "sudo pip3 install ansible"
-  ]
-}
- */
+
+
 
 
 
